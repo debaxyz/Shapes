e30dacbaf3c35f666af34baa606529132437ab8a
debaxyz <debaxyz3@gmail.com>
2026-03-14 14:57:46 +0530

    Upload

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,14 +1,7 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
-    }
 }
 
 android {
@@ -16,36 +9,70 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        targetSdk = 36
-        minSdk = 23
-
         applicationId = "com.kyant.shapes.app"
-        versionName = "1.0.0"
+        minSdk = 23
+        targetSdk = 36
         versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildFeatures {
         compose = true
     }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            vcsInfo.include = false
+        }
+    }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll(
-            "-Xlambdas=class",
-        )
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget("17"))
+        }
+    }
+    packaging {
+        resources {
+            excludes += arrayOf(
+                "DebugProbesKt.bin",
+                "kotlin-tooling-metadata.json",
+                "kotlin/**",
+                "META-INF/*.version",
+                "META-INF/**/LICENSE.txt"
+            )
+        }
+        dex {
+            useLegacyPackaging = true
+        }
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+    lint {
+        checkReleaseBuilds = false
     }
 }
 
 dependencies {
     implementation(project(":shapes"))
+   
     implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
     implementation(libs.compose.foundation)
     implementation(libs.compose.runtime)
-    implementation(libs.compose.ui)
+   
 }
--- shapes/build.gradle.kts
+++ shapes/build.gradle.kts
@@ -1,76 +1,41 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.android.multiplatform.library)
-    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.jetbrains.compose)
-    id("com.vanniktech.maven.publish")
+    alias(libs.plugins.android.library)
+   
+}
+
+android {
+    namespace = "com.kyant.shapes"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 36
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    
 }
 
 kotlin {
-    androidLibrary {
-        minSdk = 21
-        compileSdk = 36
-        namespace = "com.kyant.shapes"
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_11
-        }
-    }
-
-    jvm("desktop")
-
-    js(IR) {
-        browser()
-    }
-    wasmJs {
-        browser()
-    }
-
-    macosArm64()
-    iosArm64("iosArm64")
-    iosSimulatorArm64("iosSimulatorArm64")
-
-    sourceSets {
-        commonMain.dependencies {
-            implementation(libs.compose.ui)
-        }
-
-        all {
-            languageSettings.enableLanguageFeature("ContextParameters")
-        }
+    jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xcontext-parameters"
+        )
     }
 }
 
-mavenPublishing {
-    publishToMavenCentral()
-    signAllPublications()
-
-    coordinates("io.github.kyant0", "shapes", "1.2.0")
-
-    pom {
-        name.set("Shapes")
-        description.set("iOS-like shapes for Compose Multiplatform")
-        inceptionYear.set("2026")
-        url.set("https://github.com/Kyant0/Shapes")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("repo")
-            }
-        }
-        developers {
-            developer {
-                id.set("Kyant0")
-                name.set("Kyant")
-                url.set("https://github.com/Kyant0")
-            }
-        }
-        scm {
-            url.set("https://github.com/Kyant0/Shapes")
-            connection.set("scm:git:git://github.com/Kyant0/Shapes.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Kyant0/Shapes.git")
-        }
-    }
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    
+    
 }
+
